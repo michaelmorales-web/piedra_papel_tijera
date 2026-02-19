42c14f7f976cfa00ac4c0c0f14720bddb0c60102
michaelmorales-web <michaelmorales@colegioguanenta.edu.co>
2026-02-19 15:22:24 -0500

    Se actualiza el archivo con diseño y analisis

--- piedra_papel_tijera.py
+++ piedra_papel_tijera.py
@@ -7,10 +7,13 @@ import random
 # input
 #------
 print("------------------------")
-print("-----Vamos a jugar------")
+print("-----PIEDRA PAPEL O TIJERA------")
 print("------------------------")
 print("La computadora ha generado un número entre 1 y 3")
 print("-----Escoge entre 1 y 3-----")
+print("----El número 1 significa PIEDRA----")
+print("----El número 2 significa PAPEL----")
+print("----El número 3 significa TIJERAS----")
 computer_number = random.randint(1,3)
 
 user_number = int(input("Escoge bien: "))
@@ -19,6 +22,29 @@ user_number = int(input("Escoge bien: "))
 # procesing
 #----------
 
-# Verificar si el número ingresado este entre 1 y 3
-if(user_number>=1 and user_number<=3):
-    print
+if user_number == 1:
+    if computer_number == 1:
+        print("EMPATE")
+    elif computer_number == 2:
+        print("PERDIO")
+    else:
+        print("GANO")
+elif user_number == 2:
+    if computer_number == 1:
+        print("GANO")
+    elif computer_number == 2:
+        print("EMPATE")
+    else:
+        print("PERDIO")
+elif user_number == 3:
+    if computer_number == 1:
+        print("PERDIO")
+    elif computer_number == 2:
+        print("GANO")
+    else:
+        print("EMPATE")
+else:
+    print("ERROR")
+    exit()
+print("____________________________________________________")
+print("____________________________________________________")
